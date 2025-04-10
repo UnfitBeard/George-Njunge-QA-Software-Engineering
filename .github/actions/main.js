fb79d0d88f0c7394d3f77a534345f92db98e7297
Mr.Nobody <njunge995@gmail.com>
2025-04-10 06:24:58 +0300

    Latest updates workflow2

--- .github/actions/main.js
+++ .github/actions/main.js
@@ -1,24 +1,26 @@
-const core = require('@actions/core')//inputs nd outputs
-const exec = require('@actions/exec')//uploading to s3
+const core = require('@actions/core');
+const exec = require('@actions/exec');
 
+async function run() {
+  try {
+    // Get inputs
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region') || 'us-east-1'; // Default to 'us-east-1' if not provided
+    const distFolder = core.getInput('dist-folder', { required: true });
 
-function run() {
-  //get inputs
+    // Upload files to S3 bucket
+    const s3URI = `s3://${bucket}`;
+    await exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`);
 
-  const bucket = core.getInput('bucket', {required: true})
-  const bucketRegion = core.getInput('bucket-region', {required: true})
-  const distFolder = core.getInput('dist-folder', {required: true})
-  //Upload files to s3 bucket
+    // Get the website URL
+    const websiteURL = `https://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
 
+    // Set output
+    core.setOutput('website-url', websiteURL);
 
-  const s3URI = `s3://${bucket}`
-  exec.exec(`aws s3 sync ${distFolder} ${s3URI} --region ${bucketRegion}`)
-
-  //Get Url
-  const websiteURL = `https://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
-  // http://dkcohort.s3-website-us-east-1.amazonaws.com
-
-  core.setOutput('website-url', websiteURL)
-
+  } catch (error) {
+    core.setFailed(`Action failed with error: ${error.message}`);
+  }
 }
-run()
+
+run();
